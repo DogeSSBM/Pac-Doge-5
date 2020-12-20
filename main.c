706c3038c2c38d47d101b67a67b960749698ed9b
DogeSSBM <dogessbm@gmail.com>
2020-12-20 17:24:55 -0500

    Automatic commit.

--- main.c
+++ main.c
@@ -5,10 +5,22 @@ int main(int argc, char const *argv[])
 	const Length window = {800, 600};
 	init(window);
 	setFontSize(48);
+	Coord pos = coordDiv(window, 2);
+	const SDL_Scancode dirKey[4] = {
+		SDL_SCANCODE_W,
+		SDL_SCANCODE_D,
+		SDL_SCANCODE_S,
+		SDL_SCANCODE_A
+	};
 
 	while(1){
 		Ticks frameStart = getTicks();
 		clear();
+
+		for(Direction dir = DIR_U; dir <= DIR_L; dir++)
+			pos = coordShift(pos, dir, keyState(dirKey[dir])?4:0);
+
+		drawTextCenteredCoord(pos, "DogeLib :3");
 
 		draw();
 		events(frameStart + TPF);
