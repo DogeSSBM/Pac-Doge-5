deca6b5c9c5fbc406f0920e3d7185c94a1035fbf
DogeSSBM <dogessbm@gmail.com>
2020-12-27 15:32:15 -0500

    Automatic commit.

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 typedef struct{
 	Coord pos;
 	uint scale;
+	Img *sprite;
 	Direction dir;
 	bool power;
 	Ticks powerEnd;
@@ -53,33 +54,22 @@ bool traversable(const Coord tpos, const Map map)
 	if(!inBound(tpos.x, 0, map.len.x) || !inBound(tpos.y, 0, map.len.y))
 		return false;
 	const char c = map.text[tpos.x][tpos.y];
-	if(c==' '||c=='.'||c=='o'||c=='f'||c=='F'||c=='P'||(c >= '0' && c <= '9'))
-		return true;
-	return false;
+	if(c=='#'||c=='W')
+		return false;
+	return true;
 }
 
 Bool4 validAdjDir(const Pac pac, const Map map)
 {
-	char buf[] = " ";
 	Bool4 adjt = {0};
-	for(uint i = 0; i < 4; i++){
-		const Coord t = coordShift(wposToTpos(pac), i, 1);
-		adjt.arr[i] = traversable(t, map);
-		setFontColor(adjt.arr[i]?GREEN:RED);
-		sprintf(buf, "%c", map.text[t.x][t.y]);
-		drawTextCenteredCoord(coordShift(pac.pos,i,map.scale*2), buf);
-	}
+	for(uint i = 0; i < 4; i++)
+		adjt.arr[i] = traversable(coordShift(wposToTpos(pac), i, 1), map);
 	return adjt;
-}
-
-Coord tileOffset(const Pac pac)
-{
-	return coordMod(coordAdd(pac.pos, pac.scale/2), pac.scale);
 }
 
 bool atIntersection(const Pac pac)
 {
-	const Coord mod = tileOffset(pac);
+	const Coord mod = coordMod(coordAdd(pac.pos, pac.scale/2), pac.scale);
 	if(mod.x==0 && mod.y==0)
 		return true;
 	return false;
@@ -110,8 +100,6 @@ Pac movePac(Pac pac, const Map map)
 	const Bool4 validDir = validAdjDir(pac, map);
 	const Bool4 dirKeys = getDirExKeys();
 	const bool intersection = atIntersection(pac);
-	const Coord mod = tileOffset(pac);
-	drawDbg(pac, validDir, dirKeys, atIntersection(pac));
 	const Direction oldDir = pac.dir;
 	if(dirKeys.arr[dirINV(pac.dir)] && validDir.arr[dirINV(pac.dir)])
 		pac.dir = dirINV(pac.dir);
@@ -121,26 +109,16 @@ Pac movePac(Pac pac, const Map map)
 		if(dirKeys.arr[dirROR(pac.dir)] && validDir.arr[dirROR(pac.dir)])
 			pac.dir = dirROR(pac.dir);
 	}
-	char buf[32] = {0};
-	// setFontColor(local?GREEN:RED);
-	setFontColor(WHITE);
-	sprintf(buf, "(% 3d,% 3d)", mod.x, mod.y);
-	drawTextCoord((Coord){16,16}, buf);
 
-	static bool fadv = false;
-	if(keyPressed(SDL_SCANCODE_P))
-		fadv = !fadv;
-	if(!fadv || keyPressed(SDL_SCANCODE_SPACE)){
-		if(validDir.arr[pac.dir] || (!intersection&&dirAXP(oldDir, pac.dir)))
-			pac.pos = coordShift(pac.pos, pac.dir, 1);
-	}
+	if(validDir.arr[pac.dir] || (!intersection&&dirAXP(oldDir,pac.dir)))
+		pac.pos = coordShift(pac.pos, pac.dir, 1);
 	return pac;
 }
 
 void drawPac(const Pac pac)
 {
 	setColor(YELLOW);
-	fillCircleCoord(pac.pos, pac.scale - pac.scale/4);
+	fillCircleCoord(pac.pos, pac.scale/2);
 	setColor(WHITE);
 	drawLineCoords(pac.pos, coordShift(pac.pos, pac.dir, pac.scale - pac.scale/4));
 }
@@ -156,7 +134,7 @@ int main(int argc, char const *argv[])
 	Pac pac = {
 		.pos = tposToWposm(getSpawnCoord(map), map.scale),
 		.scale = map.scale,
-		.dir = DIR_L,
+		.dir = getSpawnDir(map,getSpawnCoord(map)),
 		.power = false,
 		.powerEnd = 0,
 		.frozen = false,
@@ -167,13 +145,10 @@ int main(int argc, char const *argv[])
 		Ticks frameStart = getTicks();
 		clear();
 
-		// for(Direction dir = DIR_U; dir <= DIR_L; dir++)
-		// 	pos = coordShift(pos, dir, keyState(dirKey[dir])?4:0);
-
-		// drawTextCenteredCoord(pos, "DogeLib :3");
 		pac = movePac(pac, map);
 		drawMap(map);
-		// drawPac(pac);
+		drawPac(pac);
+
 		draw();
 		events(frameStart + TPF);
 	}
